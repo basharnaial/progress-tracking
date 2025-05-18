ab58636776f676d0ddabbe07c05adb1843c726a5
Bashar Naial <4310757@upm.edu.sa>
2025-05-18 21:00:45 +0300

    Create Logs , Disable Capatcha , Create Attack

---
+++ src/main/java/com/secure/logging/RequestLoggingFilter.java
@@ -0,0 +1,25 @@
+package com.secure.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        logger.info("Incoming request: {} {} from IP: {}", req.getMethod(), req.getRequestURI(), req.getRemoteAddr());
+
+        chain.doFilter(request, response);
+    }
+} 
